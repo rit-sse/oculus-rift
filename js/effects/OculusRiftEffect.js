bfd8fcb789145774de3effc4782c667ef638d0fe
weswigham <wwigham@gmail.com>
2014-04-04 21:16:31 -0400

    newwer effect version, working rift effect

--- js/effects/OculusRiftEffect.js
+++ js/effects/OculusRiftEffect.js
@@ -1,179 +1,207 @@
 /**
- * Copyright 2013 Google Inc. All Rights Reserved.
+ * @author troffmo5 / http://github.com/troffmo5
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Portions come from the Oculus SDK.
- * @author benvanik
+ * Effect to render the scene in stereo 3d side by side with lens distortion.
+ * It is written to be used with the Oculus Rift (http://www.oculusvr.com/) but
+ * it works also with other HMD using the same technology
  */
 
+THREE.OculusRiftEffect = function ( renderer, options ) {
+	// worldFactor indicates how many units is 1 meter
+	var worldFactor = (options && options.worldFactor) ? options.worldFactor: 1.0;
 
-/**
- * Oculus Rift effect.
- * @param {!THREE.WebGLRenderer} renderer Target.
- * @constructor
- */
-THREE.OculusRiftEffect = function(renderer) {
-  /**
-   * Target renderer.
-   * @type {!THREE.WebGLRenderer}
-   * @private
-   */
-  this.renderer_ = renderer;
+	// Specific HMD parameters
+	var HMD = (options && options.HMD) ? options.HMD: {
+		// Parameters from the Oculus Rift DK1
+		hResolution: 1280,
+		vResolution: 800,
+		hScreenSize: 0.14976,
+		vScreenSize: 0.0936,
+		interpupillaryDistance: 0.064,
+		lensSeparationDistance: 0.064,
+		eyeToScreenDistance: 0.041,
+		distortionK : [1.0, 0.22, 0.24, 0.0],
+		chromaAbParameter: [ 0.996, -0.004, 1.014, 0.0]
+	};
 
-  /**
-   * Whether a real device is attached.
-   * @type {boolean}
-   * @private
-   */
-  this.present_ = false;
+	// Perspective camera
+	var pCamera = new THREE.PerspectiveCamera();
+	pCamera.matrixAutoUpdate = false;
+	pCamera.target = new THREE.Vector3();
 
-  /**
-   * Stereo renderer.
-   * @type {!vr.StereoRenderer}
-   * @private
-   */
-  this.stereoRenderer_ = new vr.StereoRenderer(renderer.getContext(), {
-    alpha: false,
-    depth: true,
-    stencil: false
-  });
+	// Orthographic camera
+	var oCamera = new THREE.OrthographicCamera( -1, 1, 1, -1, 1, 1000 );
+	oCamera.position.z = 1;
 
-  /**
-   * Dummy VR state used when none is available.
-   * @type {!vr.State}
-   * @private
-   */
-  this.dummyState_ = new vr.State();
+	// pre-render hooks
+	this.preLeftRender = function() {};
+	this.preRightRender = function() {};
 
-  /**
-   * Eye camera.
-   * @type {!THREE.Camera}
-   * @private
-   */
-  this.eyeCamera_ = new THREE.PerspectiveCamera();
-  this.eyeCamera_.matrixAutoUpdate = false;
+	renderer.autoClear = false;
+	var emptyColor = new THREE.Color("black");
 
-  // Initialize the renderer (with defaults).
-  this.init_(vr.HmdInfo.DEFAULT);
-};
+	// Render target
+	var RTParams = { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBAFormat };
+	var renderTarget = new THREE.WebGLRenderTarget( 640, 800, RTParams );
+	var RTMaterial = new THREE.ShaderMaterial( {
+		uniforms: {
+			"texid": { type: "t", value: renderTarget },
+			"scale": { type: "v2", value: new THREE.Vector2(1.0,1.0) },
+			"scaleIn": { type: "v2", value: new THREE.Vector2(1.0,1.0) },
+			"lensCenter": { type: "v2", value: new THREE.Vector2(0.0,0.0) },
+			"hmdWarpParam": { type: "v4", value: new THREE.Vector4(1.0,0.0,0.0,0.0) },
+			"chromAbParam": { type: "v4", value: new THREE.Vector4(1.0,0.0,0.0,0.0) }
+		},
+		vertexShader: [
+			"varying vec2 vUv;",
+			"void main() {",
+			" vUv = uv;",
+			"	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+			"}"
+		].join("\n"),
 
+		fragmentShader: [
+			"uniform vec2 scale;",
+			"uniform vec2 scaleIn;",
+			"uniform vec2 lensCenter;",
+			"uniform vec4 hmdWarpParam;",
+			'uniform vec4 chromAbParam;',
+			"uniform sampler2D texid;",
+			"varying vec2 vUv;",
+			"void main()",
+			"{",
+			"  vec2 uv = (vUv*2.0)-1.0;", // range from [0,1] to [-1,1]
+			"  vec2 theta = (uv-lensCenter)*scaleIn;",
+			"  float rSq = theta.x*theta.x + theta.y*theta.y;",
+			"  vec2 rvector = theta*(hmdWarpParam.x + hmdWarpParam.y*rSq + hmdWarpParam.z*rSq*rSq + hmdWarpParam.w*rSq*rSq*rSq);",
+			'  vec2 rBlue = rvector * (chromAbParam.z + chromAbParam.w * rSq);',
+			"  vec2 tcBlue = (lensCenter + scale * rBlue);",
+			"  tcBlue = (tcBlue+1.0)/2.0;", // range from [-1,1] to [0,1]
+			"  if (any(bvec2(clamp(tcBlue, vec2(0.0,0.0), vec2(1.0,1.0))-tcBlue))) {",
+			"    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
+			"    return;}",
+			"  vec2 tcGreen = lensCenter + scale * rvector;",
+			"  tcGreen = (tcGreen+1.0)/2.0;", // range from [-1,1] to [0,1]
+			"  vec2 rRed = rvector * (chromAbParam.x + chromAbParam.y * rSq);",
+			"  vec2 tcRed = lensCenter + scale * rRed;",
+			"  tcRed = (tcRed+1.0)/2.0;", // range from [-1,1] to [0,1]
+			"  gl_FragColor = vec4(texture2D(texid, tcRed).r, texture2D(texid, tcGreen).g, texture2D(texid, tcBlue).b, 1);",
+			"}"
+		].join("\n")
+	} );
 
-/**
- * Initializes the scene for rendering.
- * This is called whenever the device changes.
- * @param {!vr.HmdInfo} info HMD info.
- * @private
- */
-THREE.OculusRiftEffect.prototype.init_ = function(info) {
-  this.renderer_.autoClear = false;
-  this.renderer_.setSize(info.resolutionHorz, info.resolutionVert);
-};
+	var mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), RTMaterial );
 
+	// Final scene
+	var finalScene = new THREE.Scene();
+	finalScene.add( oCamera );
+	finalScene.add( mesh );
 
-/**
- * Gets the current interpupillary distance value.
- * @return {number} IPD value.
- */
-THREE.OculusRiftEffect.prototype.getInterpupillaryDistance = function() {
-  return this.stereoRenderer_.getInterpupillaryDistance();
-};
+    var left = {}, right = {};
+    var distScale = 1.0;
+	this.setHMD = function(v) {
+		HMD = v;
+		// Compute aspect ratio and FOV
+		var aspect = HMD.hResolution / (2*HMD.vResolution);
 
+		// Fov is normally computed with:
+		//   THREE.Math.radToDeg( 2*Math.atan2(HMD.vScreenSize,2*HMD.eyeToScreenDistance) );
+		// But with lens distortion it is increased (see Oculus SDK Documentation)
+		var r = -1.0 - (4 * (HMD.hScreenSize/4 - HMD.lensSeparationDistance/2) / HMD.hScreenSize);
+		distScale = (HMD.distortionK[0] + HMD.distortionK[1] * Math.pow(r,2) + HMD.distortionK[2] * Math.pow(r,4) + HMD.distortionK[3] * Math.pow(r,6));
+		var fov = THREE.Math.radToDeg(2*Math.atan2(HMD.vScreenSize*distScale, 2*HMD.eyeToScreenDistance));
 
-/**
- * Sets a new interpupillary distance value.
- * @param {number} value New IPD value.
- */
-THREE.OculusRiftEffect.prototype.setInterpupillaryDistance = function(value) {
-  this.stereoRenderer_.setInterpupillaryDistance(value);
-};
+		// Compute camera projection matrices
+		var proj = (new THREE.Matrix4()).makePerspective( fov, aspect, 0.3, 10000 );
+		var h = 4 * (HMD.hScreenSize/4 - HMD.interpupillaryDistance/2) / HMD.hScreenSize;
+		left.proj = ((new THREE.Matrix4()).makeTranslation( h, 0.0, 0.0 )).multiply(proj);
+		right.proj = ((new THREE.Matrix4()).makeTranslation( -h, 0.0, 0.0 )).multiply(proj);
 
+		// Compute camera transformation matrices
+		left.tranform = (new THREE.Matrix4()).makeTranslation( -worldFactor * HMD.interpupillaryDistance/2, 0.0, 0.0 );
+		right.tranform = (new THREE.Matrix4()).makeTranslation( worldFactor * HMD.interpupillaryDistance/2, 0.0, 0.0 );
 
-/**
- * Renders the scene to both eyes.
- * @param {!THREE.Scene} scene Three.js scene.
- * @param {!THREE.Camera} camera User camera. This is treated as the neck base.
- * @param {vr.VRState} vrstate VR state, if active.
- */
-THREE.OculusRiftEffect.prototype.render = function(scene, camera, vrstate) {
-  var info = vr.getHmdInfo() || vr.HmdInfo.DEFAULT;
-  var nowPresent = vrstate ? vrstate.hmd.present : false;
-  if (nowPresent != this.present_) {
-    if (nowPresent) {
-      // Connected.
-      this.present_ = true;
+		// Compute Viewport
+		left.viewport = [0, 0, HMD.hResolution/2, HMD.vResolution];
+		right.viewport = [HMD.hResolution/2, 0, HMD.hResolution/2, HMD.vResolution];
 
-      // Initialize the renderer/etc.
-      this.init_(info);
-    } else {
-      // Disconnected.
-      this.present_ = false;
-    }
-  }
+		// Distortion shader parameters
+		var lensShift = 4 * (HMD.hScreenSize/4 - HMD.lensSeparationDistance/2) / HMD.hScreenSize;
+		left.lensCenter = new THREE.Vector2(lensShift, 0.0);
+		right.lensCenter = new THREE.Vector2(-lensShift, 0.0);
 
-  // Propagate camera options.
-  var params = this.stereoRenderer_.getParams();
-  params.setZNear(camera.near);
-  params.setZFar(camera.far);
+		RTMaterial.uniforms['hmdWarpParam'].value = new THREE.Vector4(HMD.distortionK[0], HMD.distortionK[1], HMD.distortionK[2], HMD.distortionK[3]);
+		RTMaterial.uniforms['chromAbParam'].value = new THREE.Vector4(HMD.chromaAbParameter[0], HMD.chromaAbParameter[1], HMD.chromaAbParameter[2], HMD.chromaAbParameter[3]);
+		RTMaterial.uniforms['scaleIn'].value = new THREE.Vector2(1.0,1.0/aspect);
+		RTMaterial.uniforms['scale'].value = new THREE.Vector2(1.0/distScale, 1.0*aspect/distScale);
 
-  // Grab camera matrix from user.
-  // This is interpreted as the head base.
-  if (camera.matrixAutoUpdate) {
-    camera.updateMatrix();
-  }
-  var eyeWorldMatrix = camera.matrixWorld.clone();
+		// Create render target
+		if ( renderTarget ) renderTarget.dispose();
+		renderTarget = new THREE.WebGLRenderTarget( HMD.hResolution*distScale/2, HMD.vResolution*distScale, RTParams );
+		RTMaterial.uniforms[ "texid" ].value = renderTarget;
 
-  /*
-   * Simple head simulation:
-   *
-   *    Leye <--IPD--> Reye
-   *             ^
-   *             |
-   *   baseToEyeX/baseToEyeY
-   *             |
-   *           base
-   *
-   */
-  // TODO(benvanik): head sim
+	}	
+	this.getHMD = function() {return HMD};
 
-  // Rotate by Oculus data.
-  if (vrstate) {
-    var quat = new THREE.Quaternion(
-        vrstate.hmd.rotation[0],
-        vrstate.hmd.rotation[1],
-        vrstate.hmd.rotation[2],
-        vrstate.hmd.rotation[3]);
-    var rotMat = new THREE.Matrix4();
-    rotMat.setRotationFromQuaternion(quat);
-    eyeWorldMatrix.multiply(rotMat);
-  }
+	this.setHMD(HMD);	
 
-  // Shift around to the the eye center.
+	this.setSize = function ( width, height ) {
+		left.viewport = [width/2 - HMD.hResolution/2, height/2 - HMD.vResolution/2, HMD.hResolution/2, HMD.vResolution];
+		right.viewport = [width/2, height/2 - HMD.vResolution/2, HMD.hResolution/2, HMD.vResolution];
 
-  function convertMatrix(source, target) {
-    for (var n = 0; n < 16; n++) {
-      target.elements[n] = source[n];
-    }
-  };
+		renderer.setSize( width, height );
+	};
 
-  // Render eyes.
-  this.stereoRenderer_.render(vrstate || this.dummyState_, function(eye) {
-    var eyeCamera = this.eyeCamera_;
-    convertMatrix(eye.projectionMatrix, eyeCamera.projectionMatrix);
-    var viewAdjustMatrix = new THREE.Matrix4();
-    convertMatrix(eye.viewAdjustMatrix, viewAdjustMatrix);
-    eyeCamera.matrixWorld.multiplyMatrices(viewAdjustMatrix, eyeWorldMatrix);
+	this.render = function ( scene, camera ) {
+		var cc = renderer.getClearColor().clone();
 
-    this.renderer_.render(scene, this.eyeCamera_, undefined, true);
-  }, this);
+		// Clear
+		renderer.setClearColor(emptyColor);
+		renderer.clear();
+		renderer.setClearColor(cc);
+
+		// camera parameters
+		if (camera.matrixAutoUpdate) camera.updateMatrix();
+
+		// Render left
+		this.preLeftRender();
+
+		pCamera.projectionMatrix.copy(left.proj);
+
+		pCamera.matrix.copy(camera.matrix).multiply(left.tranform);
+		pCamera.matrixWorldNeedsUpdate = true;
+
+		renderer.setViewport(left.viewport[0], left.viewport[1], left.viewport[2], left.viewport[3]);
+
+		RTMaterial.uniforms['lensCenter'].value = left.lensCenter;
+		renderer.render( scene, pCamera, renderTarget, true );
+
+		renderer.render( finalScene, oCamera );
+
+		// Render right
+		this.preRightRender();
+
+		pCamera.projectionMatrix.copy(right.proj);
+
+		pCamera.matrix.copy(camera.matrix).multiply(right.tranform);
+		pCamera.matrixWorldNeedsUpdate = true;
+
+		renderer.setViewport(right.viewport[0], right.viewport[1], right.viewport[2], right.viewport[3]);
+
+		RTMaterial.uniforms['lensCenter'].value = right.lensCenter;
+
+		renderer.render( scene, pCamera, renderTarget, true );
+		renderer.render( finalScene, oCamera );
+
+	};
+
+	this.dispose = function() {
+		if ( RTMaterial ) {
+			RTMaterial.dispose();
+		}
+		if ( renderTarget ) {
+			renderTarget.dispose();
+		}
+	};
+
 };
--- js/main.js
+++ js/main.js
@@ -4,11 +4,20 @@ Physijs.scripts.ammo = '/js/ammo.js';
 
 var OculusLeapLift = function() {
   this.initScene();
+
+  var self = this;
+  var handleResize = function() {
+    self.camera.aspect = window.innerWidth / window.innerHeight;
+    self.camera.updateProjectionMatrix();
+
+    self.effect.setSize( window.innerWidth, window.innerHeight );
+  }
+  window.addEventListener( 'resize', handleResize, false );
 };
 
 OculusLeapLift.prototype.render = function() {
   this.scene.simulate(); // run physics
-  this.renderer.render( this.scene, this.camera); // render the scene
+  this.effect.render( this.scene, this.camera );
   this.requestAnimationFrame();
 };
 
@@ -16,19 +25,24 @@ OculusLeapLift.prototype.requestAnimationFrame = function() {
   var self = this;      
   requestAnimationFrame( function() {
     self.render();
-  } );
+  });
 };
 
 OculusLeapLift.prototype.initScene = function() {
-  this.renderer = new THREE.WebGLRenderer({ antialias: true });
-  this.renderer.setClearColor(0xeeeeff);
+  this.renderer = new THREE.WebGLRenderer({
+    devicePixelRatio: 1,
+    alpha: false,
+    clearColor: 0xffffff,
+    antialias: true
+  });
+  this.renderer.setClearColor(0xffffff);
   this.renderer.setSize( window.innerWidth, window.innerHeight );
-  document.querySelector( 'body' ).appendChild( this.renderer.domElement );
 
   this.scene = new Physijs.Scene();
 
+
   this.camera = new THREE.PerspectiveCamera(
-    35,
+    75,
     window.innerWidth / window.innerHeight,
     1,
     1000
@@ -36,6 +50,8 @@ OculusLeapLift.prototype.initScene = function() {
   this.camera.position.set( 60, 50, 60 );
   this.camera.lookAt( this.scene.position );
   this.scene.add( this.camera );
+
+  this.scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 
   // Box
   this.box = new Physijs.BoxMesh(
@@ -91,9 +107,31 @@ OculusLeapLift.prototype.initScene = function() {
   dirLight.shadowCameraFar = 3500;
   dirLight.shadowBias = -0.0001;
   dirLight.shadowDarkness = 0.35;
-  
+
   this.renderer.shadowMapEnabled = true;
   this.renderer.shadowMapSoft = true;
+  this.renderer.sortObjects = false;
+
+  this.effect = new THREE.OculusRiftEffect(this.renderer);
+
+  // Right Oculus Parameters are yet to be determined
+  this.effect.separation = 20;
+  this.effect.distortion = 0.1;
+  this.effect.fov = 110;
+
+  /*
+  this.controls = new THREE.OculusRiftControls( camera );
+  scene.add( this.controls.getObject() );*/
+
+  // Poll VR, if it's ready.
+  /*var polled = vr.pollState(vrstate);
+  controls.update( Date.now() - time, polled ? vrstate : null );*/
+
+  this.effect.render( this.scene, this.camera );
+
+  //time = Date.now();  
+
+  document.querySelector( 'body' ).appendChild( this.renderer.domElement );
 
   this.requestAnimationFrame();
 }
